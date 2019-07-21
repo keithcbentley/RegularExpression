1d006fe24d37a2552896d722db9f2f282d15ee78
Keith Bentley <keithcbentley@msn.com>
2019-07-21 09:43:32 -0700

    Do real matching.

--- main.py
+++ main.py
@@ -9,10 +9,35 @@ Input text window, input text from file, get input text from file directly
 Regular expression, regular expression options, match command button
 Output text window, options for output results
 
+Search, Match, Fullmatch, Split, Findall, Sub, Subn functionality options (radio buttons)
+
+Display pattern information after compile
+
+Escape functionality in Regular Expression section.  Show escaped string.
+
+Verbose flag
+Debug flag
+IgnoreCase flag
+Multiline flag
+Dotall flag
+
+No match output result separate from output text box.
+Displaying match results will become more complex.
+Match Expand results
+
+Compile by default
+
+Testing????
+
+View Model???
+
+Make into an object???
+
 
 """
 from tkinter import *
 from tkinter import ttk
+import re
 
 root = Tk()
 root.title('Regular Expression Playground')
@@ -67,12 +92,39 @@ re_frame.grid(column=0, row=1, sticky=(N,S,E,W))
 output_frame.grid(column=0, row=2, sticky=(N,S,E,W))
 
 
+def match_to_string(match):
+    if match is None:
+        return None
+    str = ''
+    print('match[0] len: ', len(match[0]))
+    str = str + match[0] + '\n'
+    for group in match.groups():
+        str = str + 'group: ' + group + '\n'
+    return str
+
+
 
 def re_execute_button_command():
+    # clear output results first
     output_text.delete('1.0', END)
+
+    # make sure we have a valid regex
+    regex = re_text.get('1.0', END)
+    regex = regex[0:-1] # tk text widget seems to append an extra \n
+    pattern = re.compile(regex)
+
+    # get the input string
     text = input_text.get('1.0', END)
+    text = text[0:-1]   # tk text widget seems to append an extra \n
+    print('text len', len(text))
+    # execute the regex
     print('execute')
-    output_text.insert('1.0', text)
+    for match in pattern.finditer(text):
+        match_as_string = match_to_string(match)
+        # output the results
+        if match_as_string is None:
+            match_as_string = 'No match'
+        output_text.insert(END, match_as_string)
 
 re_execute_button['command'] = re_execute_button_command
 
